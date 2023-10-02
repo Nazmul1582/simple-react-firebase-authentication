64e7621bc5823458f48c9fb80db79efd0fe799cc
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-02 22:59:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <section className="container mx-auto">
+        <div className="min-h-[80vh] flex justify-center items-center">
+          <h1 className="text-3xl font-bold">
+            Simple React Firebase Autentication
+          </h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
