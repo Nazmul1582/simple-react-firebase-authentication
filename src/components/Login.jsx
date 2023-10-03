e5f3a05b009773cbf03cdd972109efed4530505a
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-03 23:45:41 +0600

    divider and google login button added in login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,11 @@
+import googleLogo from "../assets/google-logo.png";
+
 const Login = () => {
   return (
     <section>
       <div className="min-h-[80vh] flex justify-center items-center">
         <div className="w-96 bg-white shadow-lg p-8 rounded-lg border border-gray-100">
-          <h2 className="text-2xl font-bold mb-5 text-center">
-            Please Login
-          </h2>
+          <h2 className="text-2xl font-bold mb-5 text-center">Please Login</h2>
           <form>
             <input
               className="w-full border border-gray-200 p-3 rounded-lg mb-5 focus:outline-0"
@@ -21,8 +21,21 @@ const Login = () => {
               placeholder="Enter your password"
               required
             />
-            <button className="w-full font-semibold bg-blue-500 text-white p-3 rounded-lg mb-5">
+            <button className="w-full font-semibold bg-blue-500 text-white p-3 rounded-lg mb-5 shadow-md">
               Login
+            </button>
+            {/* divider */}
+            <div className="flex items-center mb-5">
+              <div className="flex-grow border-t"></div>
+              <span className="mx-3 text-lg font-semibold">or</span>
+              <div className="flex-grow border-t"></div>
+            </div>
+            {/* login with google */}
+            <button className="w-full bg-white shadow-lg p-3 border border-gray-100 rounded-lg">
+              <div className="flex items-center justify-center gap-5">
+                <img className="w-8" src={googleLogo} alt="google logo" />
+                <p className="font-medium">Login With Google</p>
+              </div>
             </button>
           </form>
         </div>
