36ef271b07434e4f22b0cb5e000c824a27488ae0
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-04 00:18:43 +0600

    completed: login with google by enabled google sign-in-method and changed domain into localhost and using auth, provider, signInWithPopup method

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,22 @@
 import googleLogo from "../assets/google-logo.png";
 import githubLogo from "../assets/github-logo.png";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../firebase.config";
 
 const Login = () => {
-  return (
+
+    const handleGoogleLogin = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+    }
+
+    return (
     <section>
       <div className="min-h-[80vh] flex justify-center items-center">
         <div className="w-96 bg-white shadow-lg p-8 rounded-lg border border-gray-100">
@@ -23,6 +37,7 @@ const Login = () => {
             <button className="w-full font-semibold bg-blue-500 text-white p-3 rounded-lg mb-5 shadow-md">
               Login
             </button>
+          </form>
 
             {/* divider */}
             <div className="flex items-center mb-5">
@@ -32,7 +47,7 @@ const Login = () => {
             </div>
 
             {/* login with google or github */}
-            <button className="w-full bg-white shadow-lg p-3 border border-gray-100 rounded-lg mb-5">
+            <button onClick={handleGoogleLogin} className="w-full bg-white shadow-lg p-3 border border-gray-100 rounded-lg mb-5">
               <div className="flex items-center justify-center gap-5">
                 <img className="w-8" src={googleLogo} alt="google logo" />
                 <p className="font-medium">Login With Google</p>
@@ -42,10 +57,9 @@ const Login = () => {
             <button className="w-full bg-white shadow-lg p-3 border border-gray-100 rounded-lg">
               <div className="flex items-center justify-center gap-5">
                 <img className="w-8" src={githubLogo} alt="google logo" />
-                <p className="font-medium">Login With Google</p>
+                <p className="font-medium">Login With Github</p>
               </div>
             </button>
-          </form>
         </div>
       </div>
     </section>
