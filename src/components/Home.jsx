9e8365c6b37acb4e64616118bec57d86b62f19cd
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-04 03:07:17 +0600

    added logout functionality and after logout, redirect to login page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut();
+    navigate("/login")
+  }
 
   return (
     <section>
@@ -10,20 +17,22 @@ const Home = () => {
         <h1 className="text-3xl font-bold">
           Simple React Firebase Authentication
         </h1>
-        <div className="bg-white shadow-lg border border-gray-100 p-5 rounded-lg">
-          <img
-            className="mx-auto w-20 h-20 rounded-full mb-5"
-            src={user.photoURL}
-            alt={user.displayName}
-          />
-          <div className="text-center">
-            <h2 className="text-xl font-semibold">{user.displayName}</h2>
-            <p className="text-gray-400">{user.email}</p>
+        {user && (
+          <div className="bg-white shadow-lg border border-gray-100 p-5 rounded-lg">
+            <img
+              className="mx-auto w-20 h-20 rounded-full mb-5"
+              src={user.photoURL}
+              alt={user.displayName}
+            />
+            <div className="text-center">
+              <h2 className="text-xl font-semibold">{user.displayName}</h2>
+              <p className="text-gray-400">{user.email}</p>
+            </div>
+            <button onClick={handleLogOut} className="w-full font-semibold bg-blue-500 text-white p-3 rounded-lg mt-5 shadow-md">
+              Logout
+            </button>
           </div>
-          <button className="w-full font-semibold bg-blue-500 text-white p-3 rounded-lg mt-5 shadow-md">
-            Logout
-          </button>
-        </div>
+        )}
       </div>
     </section>
   );
