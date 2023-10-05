f9a87f71415bfad6eb38a0d639ef8a6a7ceedc1c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-05 15:46:08 +0600

    added user's name and photo in header and show login and logout button conditionally

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,35 @@
-import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
 
 const Header = () => {
+  const { user, logOut } = useContext(AuthContext);
+
   return (
     <header className="bg-blue-100">
       <div className="container mx-auto">
-        <div className="flex justify-between py-5">
+        <div className="flex justify-between items-center py-5">
           <div className="text-2xl font-bold text-blue-500">
             <h2>Logo</h2>
           </div>
           <nav>
-            <ul className="flex gap-5 font-semibold">
+            <ul className="flex items-center gap-5 font-semibold">
               <li>
                 <NavLink to="/">Home</NavLink>
               </li>
-              <li>
-                <NavLink to="/login">Login</NavLink>
-              </li>
             </ul>
           </nav>
+          {user ? (
+            <div className="flex items-center gap-3">
+              <p className="font-semibold">{user.displayName}</p>
+              <img
+                className="w-10 h-10 rounded-full"
+                src={user.photoURL}
+                alt={`photo of ${user.displayName}`}
+              />
+              <button onClick={logOut} className="btn btn-info">Logout</button>
+            </div>
+          ) : <Link to="/login"><button className="btn btn-info">Login</button></Link> }
         </div>
       </div>
     </header>
